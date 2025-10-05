9de94a71ec0e29cfa8b6e479111aa07e97adbd26
Rohan Parkes <rparkes@email.com>
2025-10-05 23:41:16 +1100

    Removed Routing module.

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -2,12 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+    providers: [importProvidersFrom(BrowserModule)]
 })
   .catch(err => console.error(err));
