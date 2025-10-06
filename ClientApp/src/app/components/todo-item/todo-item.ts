c6ae12869ef8c2eca41a91a70e12698b55831126
Rohan Parkes <rparkes@email.com>
2025-10-07 02:23:08 +1100

    Fixed problem with table formatting by binding to html element.

--- ClientApp/src/app/components/todo-item/todo-item.ts
+++ ClientApp/src/app/components/todo-item/todo-item.ts
@@ -2,19 +2,27 @@ import { Component, input, signal } from '@angular/core';
 import { ToDoItem } from '../../models/todoitem.model';
 
 @Component({
-  selector: 'app-todo-item',
+  selector: '[app-todo-item]',
   imports: [],
   templateUrl: './todo-item.html',
   styleUrl: './todo-item.css'
 })
 export class TodoItemComponent {
-   todoItem = input.required<ToDoItem>();
+    todoItem = input.required<ToDoItem>();
     isOverdue = signal(false);
     item = signal(new ToDoItem);
 
-   ngOnInit() {
-      let newItem : ToDoItem =  new ToDoItem(this.todoItem().id, this.todoItem().title, this.todoItem().creationDate, this.todoItem().completeBy, this.todoItem().completedDate, this.todoItem().isCompleted);
+    ngOnInit() {
+      let newItem : ToDoItem =  new ToDoItem(
+        this.todoItem().id, 
+        this.todoItem().title, 
+        this.todoItem().creationDate, 
+        this.todoItem().completeBy, 
+        this.todoItem().completedDate, 
+        this.todoItem().isCompleted);
+
       this.item.set(newItem);
+
        const today = new Date();
        if (this.todoItem().completeBy) {
            const completeByDate = this.todoItem().completeBy;
