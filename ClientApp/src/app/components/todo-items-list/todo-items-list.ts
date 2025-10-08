0559684d3d11ed90a361ee8d0eeda15aae1351f2
Rohan Parkes <rparkes@email.com>
2025-10-08 12:26:22 +1100

    Made Completed stye only apply to text columns.

--- ClientApp/src/app/components/todo-items-list/todo-items-list.ts
+++ ClientApp/src/app/components/todo-items-list/todo-items-list.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation, inject } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Repository } from '../../services/repository';
 import { TodoItemComponent } from "../todo-item/todo-item";
@@ -9,7 +9,8 @@ import { ToDoItem } from 'src/app/models/todoitem.model';
   selector: 'app-todo-items-list',
   imports: [TodoItemComponent],
   templateUrl: './todo-items-list.html',
-  styleUrl: './todo-items-list.css'
+  styleUrl: './todo-items-list.css',
+   encapsulation: ViewEncapsulation.None
 })
 export class TodoItemsList implements OnInit, OnDestroy {
     private todoitemsListChanged: Subscription = new Subscription();
