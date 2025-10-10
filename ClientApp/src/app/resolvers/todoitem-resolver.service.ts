19f2b3aae55529d290bc6dbc65ba63545ee653a3
Rohan Parkes <rparkes@email.com>
2025-10-10 13:47:22 +1100

    Fixed casing on interface.

--- ClientApp/src/app/resolvers/todoitem-resolver.service.ts
+++ ClientApp/src/app/resolvers/todoitem-resolver.service.ts
@@ -1,12 +1,12 @@
 ﻿import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
-import { TodoItemInfo } from '../models/todo-item';
+import { ToDoItemInfo } from '../models/todo-item';
 import { Injectable } from '@angular/core';
 import { Repository } from '../services/repository';
 
 @Injectable()
-export class ToDoItemResolver implements Resolve<TodoItemInfo> {
-    private todoitemObservable: Observable<TodoItemInfo> = new Observable<TodoItemInfo>();
+export class ToDoItemResolver implements Resolve<ToDoItemInfo> {
+    private todoitemObservable: Observable<ToDoItemInfo> = new Observable<ToDoItemInfo>();
     private todoitemSub: Subscription = new Subscription();
     private todoitemId: number = 0;
 
@@ -15,11 +15,11 @@ export class ToDoItemResolver implements Resolve<TodoItemInfo> {
     resolve(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
-    ): Observable<TodoItemInfo> | Promise<TodoItemInfo> | TodoItemInfo {
+    ): Observable<ToDoItemInfo> | Promise<ToDoItemInfo> | ToDoItemInfo {
         this.todoitemId = route.params['id'];
 
         this.todoitemObservable = new Observable(
-            (observer: { next: (arg0: TodoItemInfo) => void; complete: () => void }) => {
+            (observer: { next: (arg0: ToDoItemInfo) => void; complete: () => void }) => {
                 const that = this;
 
                 this.todoitemSub = this.repo.todoitemChanged.subscribe((c) => {
