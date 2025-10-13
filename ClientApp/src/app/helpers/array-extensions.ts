004812e11d0c5dd0efa3859fbe32cf0b65688812
Rohan Parkes <rparkes@email.com>
2025-10-13 12:58:11 +1100

    Expanded comment.

--- ClientApp/src/app/helpers/array-extensions.ts
+++ ClientApp/src/app/helpers/array-extensions.ts
@@ -1,7 +1,10 @@
 import { ToDoItem } from "../models/todoitem.model";
 
-//This is to mirror the sort order returned from the database whenever an item is added or updated on the client.
-// Stable sort.
+/*This is to mirror the sort order returned from the database whenever an item is added or updated on the client.
+* Stable sort.
+* Will sort by title, then due date.
+* Overdue items will appear at top, completed at bottom.
+*/
 if (!Array.prototype.DBSort) {
     Array.prototype.DBSort = function<T extends { dueBy: Date }>(this: ToDoItem[]): ToDoItem[] {
         return this.sort((a, b) => 
